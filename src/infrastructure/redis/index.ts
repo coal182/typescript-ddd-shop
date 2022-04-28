a461eadc12ac90d7c45a891938165179050849df
Cristian Martín <coal182@gmail.com>
2022-04-28 21:32:42 +0200

    Redis on docker

--- src/infrastructure/redis/index.ts
+++ src/infrastructure/redis/index.ts
@@ -1,5 +1,6 @@
 import RedisClient, { Redis } from 'ioredis';
 
 export const getRedisClient = (port: number, host: string, password: string): Redis => {
-  return new RedisClient(port, host, { password });
+  //return new RedisClient(port, host, { password });
+  return new RedisClient(port, host);
 };
