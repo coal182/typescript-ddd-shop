20330334e48826a6d7a741008f0c5eb5ad9d0635
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2022-05-18 16:28:04 +0200

    Config updated

--- src/config/main.ts
+++ src/config/main.ts
@@ -5,5 +5,5 @@ export default {
   REDIS_URI: process.env.REDIS_URI || 'redis://redis-16955.c278.us-east-1-4.ec2.cloud.redislabs.com:16955',
   REDIS_HOST: process.env.REDIS_HOST || 'redis-16955.c278.us-east-1-4.ec2.cloud.redislabs.com',
   REDIS_PORT: process.env.REDIS_PORT || 16955,
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'DZMxGqSdhQpEzKu9XSpH0QzhsnzEocXR',
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'password',
 };
