22859c3d02fcc19d31e7325425e1b83a4ea1b163
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2022-05-18 14:08:12 +0200

    external redis

--- src/config/main.ts
+++ src/config/main.ts
@@ -2,8 +2,8 @@ export default {
   API_PORT: process.env.API_PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user:123456@mongodb:27017/bookstore',
   DB_NAME: process.env.DB_NAME || 'bookstore',
-  REDIS_URI: process.env.REDIS_URI || 'redis://tba-redis:6379',
-  REDIS_HOST: process.env.REDIS_HOST || 'tba-redis',
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD || '123456',
+  REDIS_URI: process.env.REDIS_URI || 'redis://redis-16955.c278.us-east-1-4.ec2.cloud.redislabs.com:16955',
+  REDIS_HOST: process.env.REDIS_HOST || 'redis-16955.c278.us-east-1-4.ec2.cloud.redislabs.com',
+  REDIS_PORT: process.env.REDIS_PORT || 16955,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || 'DZMxGqSdhQpEzKu9XSpH0QzhsnzEocXR',
 };
