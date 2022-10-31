acd8100cf40b3802164c5a73b16ffe17cc9af5e7
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2022-10-31 10:38:14 +0100

    feat: new login style

--- ui/src/app/login/signin/signin.component.ts
+++ ui/src/app/login/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -10,18 +10,30 @@ import { AuthService } from './../../shared/auth/auth.service';
 })
 export class SigninComponent implements OnInit {
   signinForm: FormGroup;
-  constructor(public fb: FormBuilder, public authService: AuthService, public router: Router) {
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthService,
+    public router: Router,
+    private renderer2: Renderer2
+  ) {
     this.signinForm = this.fb.group({
       email: [''],
       password: [''],
     });
   }
   ngOnInit() {
+    this.renderer2.addClass(document.body.parentElement, 'animated_bg');
+    this.renderer2.addClass(document.body, 'animated_bg');
     if (this.authService.isLoggedIn) {
       this.router.navigate(['products']);
     }
   }
   loginUser() {
     this.authService.signIn(this.signinForm.value);
+  }
+
+  ngOnDestroy() {
+    this.renderer2.removeClass(document.body.parentElement, 'animated_bg');
+    this.renderer2.removeClass(document.body, 'animated_bg');
   }
 }
