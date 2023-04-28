648d7ccb6c049b1c14e811da9d6b152752a52fcc
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-04-28 13:11:47 +0200

    order cancelled migration

--- src/contexts/shop/order/domain/events/order-cancelled.ts
+++ src/contexts/shop/order/domain/events/order-cancelled.ts
@@ -1,13 +1,29 @@
-import { Event } from '@core/event';
-import { IEvent } from '@core/i-event';
+import { DomainEvent } from '@shared/domain/domain-event';
 
-import { OrderStatusEnum } from '../order-status';
+type OrderCancelledDomainEventData = object;
 
-export class OrderCancelled extends Event implements IEvent {
-  eventName = OrderCancelled.name;
-  aggregateName = 'order';
+export class OrderCancelled extends DomainEvent {
+  static readonly EVENT_NAME = 'order.cancelled';
 
-  constructor(public guid: string, public status: OrderStatusEnum) {
-    super();
+  constructor({ aggregateId, eventId, occurredOn }: { aggregateId: string; eventId?: string; occurredOn?: Date }) {
+    super({ eventName: OrderCancelled.EVENT_NAME, aggregateId, eventId, occurredOn });
+  }
+
+  toPrimitives(): OrderCancelledDomainEventData {
+    return {};
+  }
+
+  static fromPrimitives(params: {
+    aggregateId: string;
+    data: OrderCancelledDomainEventData;
+    eventId: string;
+    occurredOn: Date;
+  }): DomainEvent {
+    const { aggregateId, occurredOn, eventId } = params;
+    return new OrderCancelled({
+      aggregateId,
+      eventId,
+      occurredOn,
+    });
   }
 }
