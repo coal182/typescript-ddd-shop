adddf5a651b3366ad410a88749a32d35b5191215
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-07-05 16:27:58 +0200

    product tests refactor

--- test/contexts/shop/backend/product/application/command-handlers/update-product-description-command-handler.spec.ts
+++ test/contexts/shop/backend/product/application/command-handlers/update-product-description-command-handler.spec.ts
@@ -1,17 +1,18 @@
 import 'reflect-metadata';
 
 import { ProductCreator } from '@storeback/product/application/create/product-creator';
-import { ProductCreated } from '@storeback/product/domain/events/product-created';
 import { ProductDescriptionChanged } from '@storeback/product/domain/events/product-description-changed';
 import { CreateProductCommandHandler } from 'src/contexts/shop/product/application/command-handlers/create-product-command-handler';
 import { UpdateProductDescriptionCommandHandler } from 'src/contexts/shop/product/application/command-handlers/update-product-description-command-handler';
-import { CreateProductCommand } from 'src/contexts/shop/product/application/commands/create-product';
 import { UpdateProductDescriptionCommand } from 'src/contexts/shop/product/application/commands/update-product-description';
 import EventBusMock from 'test/contexts/shared/domain/event-bus-mock';
 
 import { ProductEventStoreMock } from '../../__mocks__/product-event-store-mock';
+import { ProductCreatedDomainEventMother } from '../../domain/product-created-domain-event-mother';
 import { ProductDescriptionMother } from '../../domain/product-description-mother';
 import { ProductMother } from '../../domain/product-mother';
+
+import { CreateProductCommandMother } from './create-product-command-mother';
 
 describe(UpdateProductDescriptionCommandHandler.name, () => {
   let eventStore: ProductEventStoreMock;
@@ -28,35 +29,16 @@ describe(UpdateProductDescriptionCommandHandler.name, () => {
     updateHandler = new UpdateProductDescriptionCommandHandler(eventBus, eventStore);
   });
   describe('when a product exists', () => {
-    const product = ProductMother.random();
+    const command = CreateProductCommandMother.random();
+    const product = ProductMother.from(command);
     const updatedDescription = ProductDescriptionMother.random();
     const domainEvents = [
-      new ProductCreated({
-        aggregateId: product.id.value,
-        name: product.name.value,
-        description: product.description.value,
-        image: product.image.value,
-        price: product.price.value,
-        brand: product.brand.value,
-        category: product.category.value,
-        ean: product.ean.value,
-      }),
+      ProductCreatedDomainEventMother.fromProduct(product),
       new ProductDescriptionChanged({
         aggregateId: product.id.value,
         description: updatedDescription.value,
       }),
     ];
-
-    const command = new CreateProductCommand(
-      product.id.value,
-      product.name.value,
-      product.description.value,
-      product.image.value,
-      product.price.value,
-      product.brand.value,
-      product.category.value,
-      product.ean.value
-    );
 
     beforeEach(async () => {
       await handler.handle(command);
--- test/contexts/shop/backend/product/application/command-handlers/update-product-image-command-handler.spec.ts
+++ test/contexts/shop/backend/product/application/command-handlers/update-product-image-command-handler.spec.ts
@@ -2,16 +2,17 @@ import 'reflect-metadata';
 
 import { UpdateProductImageCommand } from '@storeback/product/application/commands/update-product-image';
 import { ProductCreator } from '@storeback/product/application/create/product-creator';
-import { ProductCreated } from '@storeback/product/domain/events/product-created';
 import { ProductImageChanged } from '@storeback/product/domain/events/product-image-changed';
 import { CreateProductCommandHandler } from 'src/contexts/shop/product/application/command-handlers/create-product-command-handler';
 import { UpdateProductImageCommandHandler } from 'src/contexts/shop/product/application/command-handlers/update-product-image-command-handler';
-import { CreateProductCommand } from 'src/contexts/shop/product/application/commands/create-product';
 import EventBusMock from 'test/contexts/shared/domain/event-bus-mock';
 
 import { ProductEventStoreMock } from '../../__mocks__/product-event-store-mock';
+import { ProductCreatedDomainEventMother } from '../../domain/product-created-domain-event-mother';
 import { ProductImageMother } from '../../domain/product-image-mother';
 import { ProductMother } from '../../domain/product-mother';
+
+import { CreateProductCommandMother } from './create-product-command-mother';
 
 describe(UpdateProductImageCommandHandler.name, () => {
   let eventStore: ProductEventStoreMock;
@@ -28,35 +29,16 @@ describe(UpdateProductImageCommandHandler.name, () => {
     updateHandler = new UpdateProductImageCommandHandler(eventBus, eventStore);
   });
   describe('when a product exists', () => {
-    const product = ProductMother.random();
+    const command = CreateProductCommandMother.random();
+    const product = ProductMother.from(command);
     const updatedImage = ProductImageMother.random();
     const domainEvents = [
-      new ProductCreated({
-        aggregateId: product.id.value,
-        name: product.name.value,
-        description: product.description.value,
-        image: product.image.value,
-        price: product.price.value,
-        brand: product.brand.value,
-        category: product.category.value,
-        ean: product.ean.value,
-      }),
+      ProductCreatedDomainEventMother.fromProduct(product),
       new ProductImageChanged({
         aggregateId: product.id.value,
         image: updatedImage.value,
       }),
     ];
-
-    const command = new CreateProductCommand(
-      product.id.value,
-      product.name.value,
-      product.description.value,
-      product.image.value,
-      product.price.value,
-      product.brand.value,
-      product.category.value,
-      product.ean.value
-    );
 
     beforeEach(async () => {
       await handler.handle(command);
