4b5dd46febe595eae397ec7acee52994243c20ee
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-09-01 15:11:43 +0200

    cart counter top bar

--- src/apps/shop/frontend/src/app/top-bar/top-bar.component.ts
+++ src/apps/shop/frontend/src/app/top-bar/top-bar.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, switchMap, takeUntil } from 'rxjs';
 import Swal from 'sweetalert2';
 
 import { AuthService } from '../shared/auth/auth.service';
 import { LoginActions } from '../store/login/login.actions';
 import { LoginSelectors } from '../store/login/login.selectors';
 import { LoadingStatus } from '../store/metadata-types';
+import { Cart } from '../cart/interfaces/cart';
+import { HttpCartService } from '../cart/services/http-cart.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -18,12 +20,34 @@ export class TopBarComponent implements OnDestroy {
   private onDestroy$ = new Subject<void>();
 
   public user_id = '';
+  public cart_counter = 0;
 
-  constructor(public authService: AuthService, public store: Store, public router: Router) {}
+  constructor(public authService: AuthService, public store: Store, public router: Router, public cartService: HttpCartService) {}
   ngOnInit() {
     this.user_id = localStorage.getItem('user_id');
     this.store.select((state) => state).subscribe((data) => console.log('store:', data));
+    
 
+    this.loadCart();
+
+    this.loadUserMenu();
+  }
+
+  loadCart(){
+    this.cartService
+      .getItems()
+      .pipe(
+        takeUntil(this.onDestroy$),
+        switchMap(() => {
+          return this.cartService.getCart().pipe(takeUntil(this.onDestroy$));
+        })
+      )
+      .subscribe((cart: Cart) => {
+        this.cart_counter = cart.items.length;
+      });
+  }
+
+  loadUserMenu(){
     this.store.pipe(select(LoginSelectors.selectLogin), takeUntil(this.onDestroy$)).subscribe((login) => {
       if (login.metadata.loadingStatus === LoadingStatus.Loaded) {
         if (login.access_token) {
