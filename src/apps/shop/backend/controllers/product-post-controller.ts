ef9f8d1ada43f2fcabcd5d8d482b7c4ec39fcfba
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-09-01 12:18:11 +0200

    request params parser

--- src/apps/shop/backend/controllers/product-post-controller.ts
+++ src/apps/shop/backend/controllers/product-post-controller.ts
@@ -1,64 +1,69 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { Type, array, number, string, type, undefined, union } from 'io-ts';
+import { Logger } from 'winston';
 
+import { WrongRequestDataError } from '@domain/errors/wrong-request-data-error';
+import { IdProvider } from '@domain/id-provider';
+import { ParamsParser } from '@domain/params-parser';
 import { CommandBus } from '@shared/domain/command-bus';
 import { CreateProductCommand } from 'src/contexts/shop/product/application/commands/create-product';
 
 export type ProductPostRequest = Request & {
-  body: {
-    id?: string;
-    name: string;
-    description: string;
-    images: string[];
-    price: number;
-    brand: string;
-    category: string;
-    ean: string;
-  };
+  body: CreateProductParams;
+};
+
+export type CreateProductParams = {
+  id: string | undefined;
+  name: string;
+  description: string;
+  images: string[];
+  price: number;
+  brand: string;
+  category: string;
+  ean: string;
 };
 
 export class ProductPostController {
-  constructor(private commandBus: CommandBus) {}
+  constructor(private commandBus: CommandBus, private logger: Logger) {}
 
   async run(req: Request<ProductPostRequest>, res: Response) {
     try {
-      const { id, name, description, images, price, brand, category, ean } = req.body;
-      const createProductCommand = new CreateProductCommand(id, name, description, images, price, brand, category, ean);
+      const { id, name, description, images, price, brand, category, ean } = ParamsParser.parse<CreateProductParams>(
+        req.body,
+        this.createProductCodec
+      );
+
+      const createProductCommand = new CreateProductCommand(
+        id ?? IdProvider.getId(),
+        name,
+        description,
+        images,
+        price,
+        brand,
+        category,
+        ean
+      );
       await this.commandBus.dispatch(createProductCommand);
       res.status(httpStatus.CREATED).send();
     } catch (error) {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).send();
+      if (error instanceof WrongRequestDataError) {
+        this.logger.error(error.message);
+        res.status(httpStatus.BAD_REQUEST).json({ message: error.message });
+      } else {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error interno del servidor' });
+      }
     }
   }
 
-  // async getBatch(req: Request, res: Response) {
-  //   const { products } = req.body;
-
-  //   const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus semper vulputate.
-  //     In id ex ac urna dictum feugiat aliquam sit amet lorem. Nunc ut eleifend massa, at ultrices nunc.
-  //     Aenean dignissim luctus magna non egestas. Praesent feugiat turpis sed tristique elementum.
-  //     Quisque pharetra imperdiet erat quis facilisis. Nam dictum venenatis mi vitae aliquam.
-  //     Morbi ac augue at nibh convallis imperdiet ac id ipsum. Etiam auctor, arcu in iaculis convallis,
-  //     neque est scelerisque metus, eu tincidunt turpis ex quis ex. Proin at nisi a sem fringilla ultrices.
-  //     Pellentesque egestas iaculis sapien, a congue dolor pretium eget. Nunc at metus tellus.
-  //     Morbi sollicitudin placerat leo, ut viverra ante semper dignissim.
-  //     Duis posuere, elit quis elementum pretium, tellus justo aliquam massa, quis iaculis metus purus non neque.
-  //     Maecenas bibendum efficitur blandit. Aenean egestas malesuada elit sit amet bibendum.`;
-
-  //   products.results.forEach((product: any) => {
-  //     const id = uuidv4();
-
-  //     const command = new CreateProductCommand(
-  //       id,
-  //       product.title,
-  //       description,
-  //       product['formats']['image/jpeg'].replace('small', 'medium'),
-  //       'r9n16bJtQlpxxrTTThEKn',
-  //       getRandomFloat(9, 40, 2)
-  //     );
-  //     this.commandBus.send(command);
-  //   });
-
-  //   return res.json(ok('Successfully created the product', undefined));
-  // }
+  createProductCodec: Type<CreateProductParams> = type({
+    id: union([string, undefined]),
+    name: string,
+    description: string,
+    images: array(string),
+    price: number,
+    brand: string,
+    category: string,
+    ean: string,
+  });
 }
---
+++ src/contexts/shared/domain/params-parser.ts
@@ -0,0 +1,18 @@
+import { Type } from 'io-ts';
+import { PathReporter } from 'io-ts/lib/PathReporter';
+
+import { WrongRequestDataError } from './errors/wrong-request-data-error';
+
+export class ParamsParser {
+  static parse<T>(params: T, codec: Type<T, T, unknown>) {
+    const result = codec.decode(params);
+
+    if (result._tag === 'Left') {
+      const errorMessages = PathReporter.report(result);
+      const errorMsg = `Invalid request data params: ${errorMessages.join(', ')}`;
+      throw new WrongRequestDataError(errorMsg);
+    }
+
+    return result.right;
+  }
+}
