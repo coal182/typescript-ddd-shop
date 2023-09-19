a743ce6c0178afd016b602f4731901f6a57076fa
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-09-19 16:45:21 +0200

    fix no cart found issue

--- src/apps/shop/backend/controllers/cart-get-by-user-controller.ts
+++ src/apps/shop/backend/controllers/cart-get-by-user-controller.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
+import { CommandBus } from '@domain/command-bus';
+import { IdProvider } from '@domain/id-provider';
 import { QueryBus } from '@shared/domain/query-bus';
-import { CartsResponse } from '@shop-backend/cart/application/cart-response';
+import { CartResponse, CartsResponse } from '@shop-backend/cart/application/cart-response';
+import { CreateCartCommand } from '@shop-backend/cart/application/commands/create-cart';
 import { SearchCartsByCriteriaQuery } from '@shop-backend/cart/application/search-by-criteria/search-carts-by-criteria-query';
 
 type FilterType = { value: string; operator: string; field: string };
 
 export class CartGetByUserController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(private readonly queryBus: QueryBus, private readonly commandBus: CommandBus) {}
 
   async run(_req: Request, res: Response) {
     const { id } = _req.params;
@@ -25,11 +28,23 @@ export class CartGetByUserController {
       undefined
     );
 
-    const response = await this.queryBus.ask<CartsResponse>(query);
+    const result = await this.queryBus.ask<CartsResponse>(query);
 
-    res
-      .status(httpStatus.OK)
-      .send({ status: httpStatus.OK, message: 'Sucessfully retrieved cart', data: response.carts[0] });
+    const cart = result.carts.length ? result.carts[0] : await this.createNewCart(id);
+
+    res.status(httpStatus.OK).send({ status: httpStatus.OK, message: 'Sucessfully retrieved cart', data: cart });
+  }
+
+  private async createNewCart(userId: string): Promise<CartResponse> {
+    const id = IdProvider.getId();
+
+    const createCommand = new CreateCartCommand(id, userId);
+    await this.commandBus.dispatch(createCommand);
+    return {
+      id,
+      userId,
+      items: [],
+    };
   }
 
   private parseFilters(params: Array<FilterType>): Array<Map<string, string>> {
