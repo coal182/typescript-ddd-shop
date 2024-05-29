e1bdc1f9917cce519beebe2f3c5ba1f64740595b
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2024-05-29 15:58:35 +0200

    fix product reviews tests

--- src/apps/shop/frontend/src/app/product-reviews/components/product-reviews/product-reviews.component.spec.ts
+++ src/apps/shop/frontend/src/app/product-reviews/components/product-reviews/product-reviews.component.spec.ts
@@ -1,204 +1,202 @@
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Component, DebugElement, NO_ERRORS_SCHEMA, NgZone } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { By } from '@angular/platform-browser';
-import { StarRatingComponent } from 'ngx-coal';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Component, DebugElement, NO_ERRORS_SCHEMA, NgZone} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {By} from '@angular/platform-browser';
+import {StarRatingComponent} from 'ngx-coal';
+import {IdProviderService} from 'src/app/shared/services/id-provider.service';
+import {PopupService} from 'src/app/shared/services/popup/popup-service';
+import {StorageService} from 'src/app/shared/services/storage.service';
+import {MockStorageService} from 'src/app/test/mock-local-storage-service';
+import {MockNgZone} from 'src/app/test/mock-ng-zone';
 
-import { IdProviderService } from 'src/app/shared/services/id-provider.service';
-import { PopupService } from 'src/app/shared/services/popup/popup-service';
-import { StorageService } from 'src/app/shared/services/storage.service';
-import { MockStorageService } from 'src/app/test/mock-local-storage-service';
-import { MockNgZone } from 'src/app/test/mock-ng-zone';
+import {ProductReviewFormData, ProductReviewsComponent} from './product-reviews.component';
 
-import { MockProductReviewsService } from '../../../test/mock-product-reviews.service';
-import { ProductReviewBody, ProductReviewsService } from '../../services/product-reviews.service';
+import {MockProductReviewsService} from '../../../test/mock-product-reviews.service';
+import {ProductReviewBody, ProductReviewsService} from '../../services/product-reviews.service';
 
-import { ProductReviewFormData, ProductReviewsComponent } from './product-reviews.component';
+describe(ProductReviewsComponent.name, () => {
+    const productId = 'product-id';
+    const userId = 'user-id';
+    const otherUserId = 'other-user-id';
+    const reviewId = 'review-id';
+    let fixture: ComponentFixture<HostTestComponent>;
+    let hostComponent: HostTestComponent;
+    let component: ProductReviewsComponent;
+    let productReviewsService: MockProductReviewsService;
+    let storageService: StorageService;
+    let popupServiceSpy: jasmine.SpyObj<PopupService>;
 
-fdescribe(ProductReviewsComponent.name, () => {
-  const productId = 'product-id';
-  const userId = 'user-id';
-  const otherUserId = 'other-user-id';
-  const reviewId = 'review-id';
-  let fixture: ComponentFixture<HostTestComponent>;
-  let hostComponent: HostTestComponent;
-  let component: ProductReviewsComponent;
-  let productReviewsService: MockProductReviewsService;
-  let storageService: StorageService;
-  let popupServiceSpy: jasmine.SpyObj<PopupService>;
+    describe('when initialize', () => {
+        describe('and there is already a current user review', () => {
+            beforeEach(async () => {
+                initTestingModule(userId);
+                await initComponent(productId);
+            });
 
-  describe('when initialize', () => {
-    describe('and there is already a current user review', () => {
-      beforeEach(async () => {
-        initTestingModule(userId);
-        await initComponent(productId);
-      });
+            it('should fetch existent reviews by product id', () => {
+                productReviewsService.assertGetHasBeenCalledWith(productId);
+            });
 
-      it('should fetch existent reviews by product id', () => {
-        productReviewsService.assertGetHasBeenCalledWith(productId);
-      });
+            it('should not show the form', () => {
+                expect(getFormElement()).toBeNull();
+            });
 
-      it('should not show the form', () => {
-        expect(getFormElement()).toBeNull();
-      });
+            describe('when click on edit review', () => {
+                beforeEach(() => {
+                    clickEditButton();
+                });
 
-      describe('when click on edit review', () => {
-        beforeEach(() => {
-          clickEditButton();
+                it('should show the form', () => {
+                    expect(getFormElement()).not.toBeNull();
+                });
+
+                it('should load the form with review id', () => {
+                    expect(component.reviewForm.value.id).toBe(reviewId);
+                });
+
+                describe('and updates form saving it', () => {
+                    const rating = 1;
+                    const comment = 'Do not buy it!';
+                    const newFormData: ProductReviewFormData = {
+                        id: reviewId,
+                        rating,
+                        comment,
+                    };
+                    beforeEach(() => {
+                        component.reviewForm.patchValue(newFormData);
+                        fixture.detectChanges();
+
+                        clickSaveButton();
+                    });
+                    it('should update the review', async () => {
+                        const expectedBody: ProductReviewBody = {
+                            id: reviewId,
+                            productId,
+                            userId,
+                            rating,
+                            comment,
+                        };
+                        productReviewsService.assertUpdateHasBeenCalledWith(expectedBody);
+                        expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('Your review has been updated', '', 'success');
+                    });
+                });
+            });
         });
 
-        it('should show the form', () => {
-          expect(getFormElement()).not.toBeNull();
-        });
+        describe('and there is not a current user review', () => {
+            beforeEach(async () => {
+                initTestingModule(otherUserId);
+                await initComponent(productId);
+            });
 
-        it('should load the form with review id', () => {
-          expect(component.reviewForm.value.id).toBe(reviewId);
-        });
+            it('should fetch existent reviews by product id', () => {
+                productReviewsService.assertGetHasBeenCalledWith(productId);
+            });
 
-        describe('and update form saving it', () => {
-          const rating = 1;
-          const comment = 'Do not buy it!';
-          const newFormData: ProductReviewFormData = {
-            id: reviewId,
-            rating,
-            comment,
-          };
-          beforeEach(() => {
-            component.reviewForm.patchValue(newFormData);
-            clickSaveButton();
-          });
-          it('should update the review', async () => {
-            const expectedBody: ProductReviewBody = {
-              id: reviewId,
-              productId,
-              userId,
-              rating,
-              comment,
-            };
-            productReviewsService.assertUpdateHasBeenCalledWith(expectedBody);
-            fixture.detectChanges();
-            await fixture.whenStable();
-            fixture.detectChanges();
-            expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('Your review has been updated', '', 'success');
-          });
+            describe('when asked to add a new review', () => {
+                const expectedBody: ProductReviewBody = {
+                    id: reviewId,
+                    productId,
+                    userId,
+                    rating: 5,
+                    comment: 'Good product',
+                };
+
+                beforeEach(() => {
+                    component.reviewForm.setValue({
+                        id: null,
+                        rating: expectedBody.rating,
+                        comment: expectedBody.comment,
+                    });
+                    clickSaveButton();
+                });
+
+                it('should send the review', () => {
+                    productReviewsService.assertCreateHasBeenCalledWith(expectedBody);
+                    fixture.detectChanges();
+                    expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('Your review has been sent', '', 'success');
+                });
+            });
+
+            describe('when some field is missing', () => {
+                beforeEach(() => {
+                    component.reviewForm.setValue({
+                        id: null,
+                        rating: 5,
+                        comment: null,
+                    });
+                    clickSaveButton();
+                });
+
+                it('should call popup service', () => {
+                    expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('The form is not valid', '', 'error');
+                });
+            });
         });
-      });
     });
 
-    describe('and there is not a current user review', () => {
-      beforeEach(async () => {
-        initTestingModule(otherUserId);
-        await initComponent(productId);
-      });
+    function initTestingModule(reviewerUserId): void {
+        productReviewsService = new MockProductReviewsService(reviewerUserId);
+        storageService = new MockStorageService();
+        popupServiceSpy = jasmine.createSpyObj('PopupService', ['open']);
 
-      it('should fetch existent reviews by product id', () => {
-        productReviewsService.assertGetHasBeenCalledWith(productId);
-      });
-
-      describe('when asked to add a new review', () => {
-        const expectedBody: ProductReviewBody = {
-          id: reviewId,
-          productId,
-          userId,
-          rating: 5,
-          comment: 'Good product',
-        };
-
-        beforeEach(() => {
-          component.reviewForm.setValue({
-            id: null,
-            rating: expectedBody.rating,
-            comment: expectedBody.comment,
-          });
-          clickSaveButton();
+        TestBed.configureTestingModule({
+            declarations: [HostTestComponent, ProductReviewsComponent, StarRatingComponent],
+            providers: [
+                {provide: ProductReviewsService, useValue: productReviewsService},
+                {provide: StorageService, useValue: storageService},
+                {provide: FormBuilder},
+                {provide: NgZone, useClass: MockNgZone},
+                {provide: PopupService, useValue: popupServiceSpy},
+                {
+                    provide: IdProviderService,
+                    useValue: {
+                        getId: (): string => reviewId,
+                    },
+                },
+            ],
+            imports: [ReactiveFormsModule, HttpClientTestingModule, TextFieldModule, MatIconModule],
+            schemas: [NO_ERRORS_SCHEMA],
         });
+    }
 
-        it('should send the review', () => {
-          productReviewsService.assertCreateHasBeenCalledWith(expectedBody);
-          fixture.detectChanges();
-          expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('Your review has been sent', '', 'success');
-        });
-      });
+    async function initComponent(productId: string): Promise<void> {
+        await TestBed.compileComponents();
 
-      describe('when some field is missing', () => {
-        beforeEach(() => {
-          component.reviewForm.setValue({
-            id: null,
-            rating: 5,
-            comment: null,
-          });
-          clickSaveButton();
-        });
+        fixture = TestBed.createComponent(HostTestComponent);
+        hostComponent = fixture.componentInstance;
+        hostComponent.productId = productId;
 
-        it('should call popup service', () => {
-          expect(popupServiceSpy.open).toHaveBeenCalledOnceWith('The form is not valid', '', 'error');
-        });
-      });
-    });
-  });
+        component = fixture.debugElement.query(By.directive(ProductReviewsComponent)).componentInstance;
 
-  function initTestingModule(reviewerUserId): void {
-    productReviewsService = new MockProductReviewsService(reviewerUserId);
-    storageService = new MockStorageService();
-    popupServiceSpy = jasmine.createSpyObj('PopupService', ['open']);
+        fixture.detectChanges();
+    }
 
-    TestBed.configureTestingModule({
-      declarations: [HostTestComponent, ProductReviewsComponent, StarRatingComponent],
-      providers: [
-        { provide: ProductReviewsService, useValue: productReviewsService },
-        { provide: StorageService, useValue: storageService },
-        { provide: FormBuilder },
-        { provide: NgZone, useClass: MockNgZone },
-        { provide: PopupService, useValue: popupServiceSpy },
-        {
-          provide: IdProviderService,
-          useValue: {
-            getId: (): string => reviewId,
-          },
-        },
-      ],
-      imports: [ReactiveFormsModule, HttpClientTestingModule, TextFieldModule, MatIconModule],
-      schemas: [NO_ERRORS_SCHEMA],
-    });
-  }
+    function getFormElement(): DebugElement {
+        return fixture.debugElement.query(By.css('[data-test="review-form"]'));
+    }
 
-  async function initComponent(productId: string): Promise<void> {
-    await TestBed.compileComponents();
+    function getSaveButton(): DebugElement {
+        return fixture.debugElement.query(By.css('[data-test="save-review-btn"]'));
+    }
 
-    fixture = TestBed.createComponent(HostTestComponent);
-    hostComponent = fixture.componentInstance;
-    hostComponent.productId = productId;
+    function clickSaveButton(): void {
+        getSaveButton().nativeElement.click();
+        fixture.detectChanges();
+    }
 
-    component = fixture.debugElement.query(By.directive(ProductReviewsComponent)).componentInstance;
+    function clickEditButton(): void {
+        fixture.debugElement.query(By.css('[data-test="edit-review-btn"]')).nativeElement.click();
+        fixture.detectChanges();
+    }
 
-    fixture.detectChanges();
-  }
-
-  function getFormElement(): DebugElement {
-    return fixture.debugElement.query(By.css('[data-test="review-form"]'));
-  }
-
-  function getSaveButton(): DebugElement {
-    return fixture.debugElement.query(By.css('[data-test="save-review-btn"]'));
-  }
-
-  function clickSaveButton(): void {
-    getSaveButton().nativeElement.click();
-    fixture.detectChanges();
-  }
-
-  function clickEditButton(): void {
-    fixture.debugElement.query(By.css('[data-test="edit-review-btn"]')).nativeElement.click();
-    fixture.detectChanges();
-  }
-
-  @Component({
-    template: '<app-product-reviews [productId]="productId"></app-product-reviews>',
-  })
-  class HostTestComponent {
-    productId = 'product-id';
-  }
+    @Component({
+        template: '<app-product-reviews [productId]="productId"></app-product-reviews>',
+    })
+    class HostTestComponent {
+        productId = 'product-id';
+    }
 });
--- src/apps/shop/frontend/src/app/product-reviews/components/product-reviews/product-reviews.component.ts
+++ src/apps/shop/frontend/src/app/product-reviews/components/product-reviews/product-reviews.component.ts
@@ -1,164 +1,161 @@
-import { Component, Input, NgZone, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
+import {Component, Input, NgZone, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {IdProviderService} from 'src/app/shared/services/id-provider.service';
+import {PopupService} from 'src/app/shared/services/popup/popup-service';
+import {StorageService} from 'src/app/shared/services/storage.service';
 
-import { IdProviderService } from 'src/app/shared/services/id-provider.service';
-import { PopupService } from 'src/app/shared/services/popup/popup-service';
-import { StorageService } from 'src/app/shared/services/storage.service';
-
-import { ProductReview } from '../../interfaces/product-reviews.interface';
-import { ProductReviewBody, ProductReviewsService } from '../../services/product-reviews.service';
+import {ProductReview} from '../../interfaces/product-reviews.interface';
+import {ProductReviewBody, ProductReviewsService} from '../../services/product-reviews.service';
 
 type ProductReviewForm = FormGroup<{
-  id: FormControl<string | null>;
-  rating: FormControl<number>;
-  comment: FormControl<string>;
+    id: FormControl<string | null>;
+    rating: FormControl<number>;
+    comment: FormControl<string>;
 }>;
 
 export interface ProductReviewFormData {
-  id: string | null;
-  rating: number;
-  comment: string;
+    id: string | null;
+    rating: number;
+    comment: string;
 }
 
 @Component({
-  selector: 'app-product-reviews',
-  templateUrl: './product-reviews.component.html',
-  styleUrls: ['./product-reviews.component.css'],
+    selector: 'app-product-reviews',
+    templateUrl: './product-reviews.component.html',
+    styleUrls: ['./product-reviews.component.css'],
 })
 export class ProductReviewsComponent implements OnInit, OnDestroy {
-  private readonly onDestroy$ = new Subject();
-  private defaultFormValue: ProductReviewFormData = { id: null, rating: 0, comment: '' };
+    private readonly onDestroy$ = new Subject();
+    private defaultFormValue: ProductReviewFormData = {id: null, rating: 0, comment: ''};
 
-  @Input({ required: true })
-  public productId: string;
+    @Input({required: true})
+    public productId: string;
 
-  public userId: string;
+    public userId: string;
 
-  public reviews: ProductReview[];
+    public reviews: ProductReview[];
 
-  public rating: FormControl<number | null> = new FormControl<number>(0, [Validators.required]);
+    public rating: FormControl<number | null> = new FormControl<number>(0, [Validators.required]);
 
-  public reviewForm: ProductReviewForm = this.fb.group({
-    id: [null],
-    rating: this.rating,
-    comment: new FormControl<string>('', [Validators.required, Validators.maxLength(1000)]),
-  });
+    public reviewForm: ProductReviewForm = this.fb.group({
+        id: [null],
+        rating: this.rating,
+        comment: new FormControl<string>('', [Validators.required, Validators.maxLength(1000)]),
+    });
 
-  public shouldShowForm = false;
+    public shouldShowForm = false;
 
-  constructor(
-    private productReviewsService: ProductReviewsService,
-    private fb: FormBuilder,
-    private ngZone: NgZone,
-    private storageService: StorageService,
-    private idProvider: IdProviderService,
-    private popupService: PopupService
-  ) {
-    this.userId = this.storageService.getItem('user_id');
-    this.reviewForm.patchValue(this.defaultFormValue);
-  }
-
-  get commentErrorMsg(): string {
-    const errors = this.reviewForm.get('comment')?.errors;
-    if (errors?.['required']) {
-      return 'Comment is mandatory';
-    } else if (errors?.['maxLength']) {
-      return 'Comment is too long, maximum is 1000';
+    constructor(
+        private productReviewsService: ProductReviewsService,
+        private fb: FormBuilder,
+        private ngZone: NgZone,
+        private storageService: StorageService,
+        private idProvider: IdProviderService,
+        private popupService: PopupService,
+    ) {
+        this.userId = this.storageService.getItem('user_id');
+        this.reviewForm.patchValue(this.defaultFormValue);
     }
 
-    return '';
-  }
+    get commentErrorMsg(): string {
+        const errors = this.reviewForm.get('comment')?.errors;
+        if (errors?.['required']) {
+            return 'Comment is mandatory';
+        } else if (errors?.['maxLength']) {
+            return 'Comment is too long, maximum is 1000';
+        }
 
-  public invalidInput(inputName: string): boolean {
-    return this.reviewForm.controls[inputName].invalid;
-  }
-
-  public ngOnInit(): void {
-    this.loadProductReviews();
-  }
-
-  public ngOnDestroy(): void {
-    this.onDestroy$.next('');
-    this.onDestroy$.complete();
-  }
-
-  public saveReview(): void {
-    if (!this.reviewForm.valid) {
-      this.reviewForm.markAllAsTouched();
-      this.popupService.open('The form is not valid', '', 'error');
-      return;
+        return '';
     }
 
-    const commonReviewParams = {
-      productId: this.productId,
-      userId: this.userId,
-      rating: this.reviewForm.value.rating,
-      comment: this.reviewForm.value.comment,
-    };
-
-    if (this.reviewForm.value.id) {
-      const updateReviewParams: ProductReviewBody = {
-        id: this.reviewForm.value.id,
-        ...commonReviewParams,
-      };
-
-      this.productReviewsService.update(updateReviewParams).subscribe({
-        next: () => {
-          this.popupService.open('Your review has been updated', '', 'success');
-          this.resetForm();
-          this.loadProductReviews();
-        },
-        error: () => {
-          this.popupService.open('There has been an error updating your review', '', 'error');
-        },
-      });
-
-      return;
+    public invalidInput(inputName: string): boolean {
+        return this.reviewForm.controls[inputName].invalid;
     }
 
-    const addReviewParams: ProductReviewBody = {
-      id: this.idProvider.getId(),
-      ...commonReviewParams,
-    };
-
-    this.productReviewsService.create(addReviewParams).subscribe({
-      next: () => {
-        this.popupService.open('Your review has been sent', '', 'success');
-        this.resetForm();
+    public ngOnInit(): void {
         this.loadProductReviews();
-      },
-      error: () => {
-        this.popupService.open('There has been an error saving your review', '', 'error');
-      },
-    });
-  }
-
-  public edit(review: ProductReview): void {
-    this.reviewForm.patchValue({ id: review.id, rating: review.rating, comment: review.comment });
-    this.shouldShowForm = true;
-  }
-
-  private resetForm(): void {
-    this.reviewForm.reset(this.defaultFormValue);
-    // Object.keys(this.reviewForm.controls).forEach((key) => {
-    //   this.reviewForm.controls[key].setErrors(null);
-    // });
-  }
-
-  private loadProductReviews(): void {
-    this.productReviewsService.get(this.productId).subscribe((res) => {
-      this.reviews = res.data;
-
-      this.handleFormVisibility();
-    });
-  }
-
-  private handleFormVisibility(): void {
-    if (this.reviews.some((review) => review.userId === this.userId)) {
-      this.shouldShowForm = false;
-    } else {
-      this.shouldShowForm = true;
     }
-  }
+
+    public ngOnDestroy(): void {
+        this.onDestroy$.next('');
+        this.onDestroy$.complete();
+    }
+
+    public saveReview(): void {
+        if (!this.reviewForm.valid) {
+            this.reviewForm.markAllAsTouched();
+            this.popupService.open('The form is not valid', '', 'error');
+            return;
+        }
+
+        const commonReviewParams = {
+            productId: this.productId,
+            userId: this.userId,
+            rating: this.reviewForm.value.rating,
+            comment: this.reviewForm.value.comment,
+        };
+
+        if (this.reviewForm.value.id) {
+            const updateReviewParams: ProductReviewBody = {
+                id: this.reviewForm.value.id,
+                ...commonReviewParams,
+            };
+
+            this.productReviewsService.update(updateReviewParams).subscribe({
+                next: () => {
+                    this.popupService.open('Your review has been updated', '', 'success');
+                    this.resetForm();
+                    this.loadProductReviews();
+                },
+                error: () => {
+                    this.popupService.open('There has been an error updating your review', '', 'error');
+                },
+            });
+        } else {
+            const addReviewParams: ProductReviewBody = {
+                id: this.idProvider.getId(),
+                ...commonReviewParams,
+            };
+
+            this.productReviewsService.create(addReviewParams).subscribe({
+                next: () => {
+                    this.popupService.open('Your review has been sent', '', 'success');
+                    this.resetForm();
+                    this.loadProductReviews();
+                },
+                error: () => {
+                    this.popupService.open('There has been an error saving your review', '', 'error');
+                },
+            });
+        }
+    }
+
+    public edit(review: ProductReview): void {
+        this.reviewForm.patchValue({id: review.id, rating: review.rating, comment: review.comment});
+        this.shouldShowForm = true;
+    }
+
+    private resetForm(): void {
+        this.reviewForm.reset(this.defaultFormValue);
+        // Object.keys(this.reviewForm.controls).forEach((key) => {
+        //   this.reviewForm.controls[key].setErrors(null);
+        // });
+    }
+
+    private loadProductReviews(): void {
+        this.productReviewsService.get(this.productId).subscribe((res) => {
+            this.reviews = res.data;
+
+            this.handleFormVisibility();
+        });
+    }
+
+    private handleFormVisibility(): void {
+        if (this.reviews.some((review) => review.userId === this.userId)) {
+            this.shouldShowForm = false;
+        } else {
+            this.shouldShowForm = true;
+        }
+    }
 }
