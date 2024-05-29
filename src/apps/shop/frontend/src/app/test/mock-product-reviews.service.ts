e5c52e2fdefce1f1344a6147114ae79284b41042
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2024-05-29 15:59:20 +0200

    fix product reviews service mock

--- src/apps/shop/frontend/src/app/test/mock-product-reviews.service.ts
+++ src/apps/shop/frontend/src/app/test/mock-product-reviews.service.ts
@@ -1,45 +1,45 @@
-import { of } from 'rxjs';
+import {of} from 'rxjs';
 
-import { ProductReviewBody, ProductReviewsService } from '../product-reviews/services/product-reviews.service';
+import {ProductReviewBody, ProductReviewsService} from '../product-reviews/services/product-reviews.service';
 
 export class MockProductReviewsService extends ProductReviewsService {
-  public constructor(private reviewerUserId: string) {
-    super();
-  }
+    public constructor(private reviewerUserId: string) {
+        super();
+    }
 
-  public get = jasmine.createSpy('get').and.callFake((productId: string) => {
-    return of({
-      status: 200,
-      message: 'Succesfully retrieved',
-      data: [
-        {
-          id: 'review-id',
-          productId: productId, // Use the productId parameter
-          userId: this.reviewerUserId,
-          rating: 5,
-          comment: 'Good product',
-        },
-      ],
+    public get = jasmine.createSpy('get').and.callFake((productId: string) => {
+        return of({
+            status: 200,
+            message: 'Succesfully retrieved',
+            data: [
+                {
+                    id: 'review-id',
+                    productId: productId, // Use the productId parameter
+                    userId: this.reviewerUserId,
+                    rating: 5,
+                    comment: 'Good product',
+                },
+            ],
+        });
     });
-  });
 
-  public create = jasmine.createSpy('create').and.callFake((body: ProductReviewBody) => {
-    return of(); // Return an observable with an empty response
-  });
+    public create = jasmine.createSpy('create').and.callFake(() => {
+        return of(() => {}); // Return an observable with an empty response
+    });
 
-  public update = jasmine.createSpy('update').and.callFake((body: ProductReviewBody) => {
-    return of(); // Return an observable with an empty response
-  });
+    public update = jasmine.createSpy('update').and.callFake(() => {
+        return of(() => {}); // Return an observable with an empty response
+    });
 
-  public assertGetHasBeenCalledWith(productId: string) {
-    expect(this.get).toHaveBeenCalledWith(productId);
-  }
+    public assertGetHasBeenCalledWith(productId: string): void {
+        expect(this.get).toHaveBeenCalledWith(productId);
+    }
 
-  public assertCreateHasBeenCalledWith(body: ProductReviewBody) {
-    expect(this.create).toHaveBeenCalledWith(body);
-  }
+    public assertCreateHasBeenCalledWith(body: ProductReviewBody): void {
+        expect(this.create).toHaveBeenCalledWith(body);
+    }
 
-  public assertUpdateHasBeenCalledWith(body: ProductReviewBody) {
-    expect(this.update).toHaveBeenCalledWith(body);
-  }
+    public assertUpdateHasBeenCalledWith(body: ProductReviewBody): void {
+        expect(this.update).toHaveBeenCalledWith(body);
+    }
 }
