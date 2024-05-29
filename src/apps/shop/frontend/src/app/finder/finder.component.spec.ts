6ba6147a3ead99d4c698962221c83886042c8ac8
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2024-05-29 16:00:45 +0200

    fix finder test

--- src/apps/shop/frontend/src/app/finder/finder.component.spec.ts
+++ src/apps/shop/frontend/src/app/finder/finder.component.spec.ts
@@ -1,29 +1,29 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatAutocomplete } from '@angular/material/autocomplete';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatAutocomplete} from '@angular/material/autocomplete';
 
-import { FinderComponent } from './finder.component';
+import {FinderComponent} from './finder.component';
 
 describe('FinderComponent', () => {
-  let component: FinderComponent;
-  let fixture: ComponentFixture<FinderComponent>;
+    let component: FinderComponent;
+    let fixture: ComponentFixture<FinderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [FinderComponent, MatAutocomplete],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, MatAutocomplete],
+            declarations: [FinderComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FinderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FinderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
