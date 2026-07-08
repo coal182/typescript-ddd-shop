af40e4d6c4fc3fd9895c97b8bb8422b6cefb3cbd
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2026-07-08 17:42:49 +0200

    feat(finder): replace mat-autocomplete with hlm-autocomplete, remove fromEvent/getElementById pattern

--- src/apps/shop/frontend/src/app/finder/finder.component.ts
+++ src/apps/shop/frontend/src/app/finder/finder.component.ts
@@ -1,64 +1,59 @@
 import {CurrencyPipe} from '@angular/common';
-import {Component, OnInit} from '@angular/core';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {Component, OnDestroy} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {fromEvent} from 'rxjs';
-import {tap, switchMap, debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {HlmAutocompleteImports} from '@spartan-ng/helm/autocomplete';
+import {HlmSpinnerImports} from '@spartan-ng/helm/spinner';
+import {Subject, debounceTime, distinctUntilChanged, switchMap, takeUntil, tap} from 'rxjs';
 
 import {Operator, Product} from '../products/interfaces/products.interface';
 import {HttpProductService} from '../products/services/http-product.service';
 import {ImagePipe} from '../shared/pipes/image.pipe';
 
-export interface State {
-    flag: string;
-    name: string;
-    population: string;
-}
-
-/**
- * @title Autocomplete overview
- */
 @Component({
     selector: 'finder',
     templateUrl: 'finder.component.html',
     styleUrls: ['finder.component.css'],
-    imports: [MatAutocompleteModule, MatInputModule, MatFormFieldModule, RouterModule, CurrencyPipe, ImagePipe, MatProgressSpinnerModule],
+    imports: [RouterModule, CurrencyPipe, ImagePipe, ...HlmAutocompleteImports, ...HlmSpinnerImports],
 })
-export class FinderComponent implements OnInit {
-    filteredProducts: Product[];
-    loading = false;
+export class FinderComponent implements OnDestroy {
+    public filteredProducts: ReadonlyArray<Product> = [];
+    public loading = false;
 
-    ngOnInit(): void {
-        const searchBoxElement = <HTMLInputElement>document.getElementById('search-box-input');
+    private readonly search$ = new Subject<string>();
+    private readonly destroy$ = new Subject<void>();
 
-        // search composed observable
-        const search$ = fromEvent(searchBoxElement, 'keyup').pipe(
-            distinctUntilChanged(),
-            debounceTime(300),
-            tap(() => (this.loading = true)),
-            switchMap(() => {
-                const paramsObj = {
-                    'filters[0][field]': 'name',
-                    'filters[0][operator]': Operator.CONTAINS,
-                    'filters[0][value]': searchBoxElement.value != '' ? searchBoxElement.value : ' ',
-                    orderBy: 'name',
-                    order: 'asc',
-                    limit: '30',
-                    offset: '0',
-                };
-                return this.productService.getProducts(paramsObj);
-            }),
-            tap(() => (this.loading = false)),
-        );
-
-        // search subscription
-        search$.subscribe((data) => {
-            this.filteredProducts = data.data;
-        });
+    constructor(public productService: HttpProductService) {
+        this.search$
+            .pipe(
+                takeUntil(this.destroy$),
+                distinctUntilChanged(),
+                debounceTime(300),
+                tap(() => (this.loading = true)),
+                switchMap((term) => {
+                    const paramsObj = {
+                        'filters[0][field]': 'name',
+                        'filters[0][operator]': Operator.CONTAINS,
+                        'filters[0][value]': term !== '' ? term : ' ',
+                        orderBy: 'name',
+                        order: 'asc',
+                        limit: '30',
+                        offset: '0',
+                    };
+                    return this.productService.getProducts(paramsObj);
+                }),
+                tap(() => (this.loading = false)),
+            )
+            .subscribe((data) => {
+                this.filteredProducts = data.data;
+            });
     }
 
-    constructor(public productService: HttpProductService) {}
+    public onSearchChange(term: string): void {
+        this.search$.next(term);
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 }
