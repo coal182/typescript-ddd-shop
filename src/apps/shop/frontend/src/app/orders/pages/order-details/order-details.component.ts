eab34445ac8ce6808525b8d6ac7ae64d36c98d73
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2026-07-08 17:49:35 +0200

    feat(order-details): replace mat-table/mat-progress-spinner with spartan table/spinner, fix address field

--- src/apps/shop/frontend/src/app/orders/pages/order-details/order-details.component.ts
+++ src/apps/shop/frontend/src/app/orders/pages/order-details/order-details.component.ts
@@ -1,25 +1,21 @@
 import {AsyncPipe, CurrencyPipe} from '@angular/common';
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTable, MatTableModule} from '@angular/material/table';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {HlmSpinnerImports} from '@spartan-ng/helm/spinner';
+import {HlmTableImports} from '@spartan-ng/helm/table';
 import {Observable} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
 
-import {Order, OrderLine} from '../../interfaces/orders';
+import {Order} from '../../interfaces/orders';
 import {HttpOrderService} from '../../services/http-order.service';
 
 @Component({
     selector: 'app-order-details',
     templateUrl: './order-details.component.html',
     styleUrls: ['./order-details.component.css'],
-    imports: [MatTableModule, AsyncPipe, CurrencyPipe, MatProgressSpinnerModule],
+    imports: [AsyncPipe, CurrencyPipe, ...HlmTableImports, ...HlmSpinnerImports],
 })
 export class OrderDetailsComponent implements OnInit {
-    public columnsToDisplay = ['name', 'qty', 'price'];
-    @ViewChild(MatTable)
-    public table!: MatTable<OrderLine>;
-
     public order$: Observable<Order> | undefined;
     public isLoading = false;
 
@@ -31,20 +27,15 @@ export class OrderDetailsComponent implements OnInit {
     public ngOnInit(): void {
         const routeParams = this.route.snapshot.paramMap;
         const orderIdFromRoute: string = routeParams.get('orderId');
-
         const params = {id: orderIdFromRoute};
 
         this.isLoading = true;
 
         this.order$ = this.orderService.getOrder(params).pipe(
             map((ord) => ord.data),
-            tap((data) => console.log(data)),
             tap(() => (this.isLoading = false)),
         );
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public cancel(order: Order): void {
-        // not implemented yet
-    }
+    public cancel(order: Order): void {}
 }
